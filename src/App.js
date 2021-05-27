2a3ab030089a319aa4c44a935db22710032e85a6
magne-matias <magnematias01@gmail.com>
2021-05-27 11:03:41 -0300

    commit hooks

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+//importando los hooks que vamo a usar
+import React,{useState, useEffect} from 'react'
+
+export default function App() {
+
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    console.log("Latitude is:", lat)
+    console.log("Longitude is:", long)
+  }, [lat, long]);
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
